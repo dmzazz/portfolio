33ec2a8e288f33dfb8829b1ac40b791615ba8058
Dimas Azizir <dimaslaylaa@gmail.com>
2024-10-24 14:25:03 +0700

    menambahkan tech stack

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -13,8 +13,9 @@ const projects = [
   {
     id: "selfcare",
     title: "SelfCare",
-    text: "Self-Care is a website-based information system so that adolescents can gain insight into psychological problems starting from learning to recognize themselves, knowing about what psychology is and the impact that will be received if ignored and understand when to consult psychology. Built using ReactJs, Bootstrap, JSON Server",
+    text: "Self-Care is a website-based information system so that adolescents can gain insight into psychological problems starting from learning to recognize themselves, knowing about what psychology is and the impact that will be received if ignored and understand when to consult psychology.",
     link: "https://github.com/dmzazz/capstone-kel-5",
+    techStack: "ReactJs, Bootstrap, JSON Server",
     linkText: "View Code",
     imgSrc: selfcare,
     aos: "fade-left",
@@ -22,8 +23,9 @@ const projects = [
   {
     id: "undangan",
     title: "Undangan Digital",
-    text: "An online invitation in the form of a website to make it easier for users to invite relatives to remote weddings or circumcisions. Built using HTML, CSS, JavaScript and Bootstrap.",
+    text: "An online invitation in the form of a website to make it easier for users to invite relatives to remote weddings or circumcisions.",
     link: "https://undangankita.vercel.app",
+    techStack: "HTML, CSS, JavaScript, Bootstrap",
     linkText: "View Project",
     imgSrc: undangan,
     aos: "fade-right",
@@ -31,8 +33,9 @@ const projects = [
   {
     id: "warehouse",
     title: "Warehouse",
-    text: "An application that functions to record incoming and outgoing goods. using several programming languages ​​such as ReactJs, NodeJs and MongoDB as Database. Tailwind for styling.",
+    text: "An application that functions to record incoming and outgoing goods.",
     link: "https://github.com/whend12/Warehouse-Stocktifity",
+    techStack: "ReactJs, NodeJs, MongoDB, TailwindCSS",
     linkText: "View Code",
     imgSrc: warehouse,
     aos: "fade-left",
@@ -51,6 +54,7 @@ const projects = [
     title: "Batch Tracker",
     text: "Provides an accurate and efficient tracking mechanism in the process of submitting and receiving work orders (WO), raw materials (RM), and production support materials (PM) from the Production Planning and Control (PPIC) division to the Warehouse, to Production.",
     link: "#batch-tracker",
+    techStack: "NextJS, DotNet, MySQL, TailwindCSS, DaisyUI",
     linkText: "Private",
     imgSrc: batchtracker,
     aos: "fade-left",
@@ -60,6 +64,7 @@ const projects = [
     title: "Lunar Studio Cikarang Design",
     text: "Design Lunar Studio Cikarang",
     link: "https://www.figma.com/proto/Fr15pvhJ5jIi57wKctDbz3/Skripsi-Ine?node-id=1-2&node-type=canvas&t=o48zliZRQXx6uuYu-1&scaling=min-zoom&content-scaling=fixed&page-id=0%3A1",
+    techStack: "Figma",
     linkText: "See Design",
     imgSrc: uilunarstudio,
     aos: "fade-left",
@@ -82,13 +87,20 @@ const Project = () => {
       {projects.map((project) => (
         <div key={project.id} className={`card ${project.id} mb-3 mt-5 border-0`} data-aos={project.aos} data-aos-duration="700" data-aos-delay="200">
           <div className="row align-items-center">
-            <div className="col-lg-7">
+            <div className="col-lg-6">
               <img src={project.imgSrc} className="img-fluid rounded-start" alt={project.title}></img>
             </div>
-            <div className="col-lg-4">
+            <div className="col-lg-5">
               <div className="card-body pb-5 px-5 p-lg-0">
                 <h3 className="card-title">{project.title}</h3>
                 <p className="card-text my-4">{project.text}</p>
+                <p className="fw-semibold">
+                  {project.techStack.split(", ").map((tech, index) => (
+                    <span key={index} className="border border-gray-400 px-2 py-1 rounded mx-1">
+                      {tech}
+                    </span>
+                  ))}
+                </p>
                 <Link to={project.link} target="_blank">
                   <button className="view-project text-light py-3 px-5 border-0 rounded">{project.linkText}</button>
                 </Link>
