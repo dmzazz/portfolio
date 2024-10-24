86a69a906e60831a1a0ccb0550edb50535330ffa
Dimas Azizir <dimaslaylaa@gmail.com>
2024-10-24 14:36:21 +0700

    merubah layout

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -13,25 +13,16 @@ const Contact = () => {
     <div className="container">
       <div className="row align-items-center flex-column-reverse flex-lg-row">
         <div className="col-lg-6 mt-5 mt-lg-0">
-          <h2 className="text-lg-end" data-aos="fade-left" data-aos-duration="800">
-            Contact Us
+          <h2 className="text-lg" data-aos="fade-left" data-aos-duration="800">
+            About
           </h2>
-          <p className="py-2 text-lg-end fs-4" data-aos="fade-left" data-aos-duration="800">
-            Want to recruit me? Send a message via email at dmzazz9@gmail.com{" "}
+          <p className="py-2 text-lg fs-4" data-aos="fade-left" data-aos-duration="800">
+            a fresh graduate who have an interest in <span className="fw-semibold">Frontend Engineer</span>
           </p>
-          <Link to="mailto:dmzazz9@gmail.com">
-            <button className="border-0 px-4 py-2 float-lg-end" data-aos="fade-left" data-aos-duration="800">
-              Let's Chat
-            </button>
-          </Link>
-        </div>
-
-        <div className="col-lg-4 text-center">
-          <img src={profile} alt="profile.png" data-aos="flip-left" data-aos-duration="900"></img>
-          <ul className="list-group list-group-horizontal justify-content-center align-items-center">
-            <li className="list-text list-group fs-5 me-2" data-aos="fade-right" data-aos-delay="200">
+          <ul className="list-group list-group-horizontal align-items-center">
+            {/* <li className="list-text list-group fs-5 me-2" data-aos="fade-right" data-aos-delay="200">
               Keep in touch
-            </li>
+            </li> */}
             <li className="list-group mx-2">
               <Link to="https://www.linkedin.com/in/dimas-azizir-261661198/" target="_blank" data-aos="fade-right" data-aos-delay="500">
                 <img src={linkedln} alt="linkedln.png" width={50}></img>
@@ -54,6 +45,10 @@ const Contact = () => {
             </li>
           </ul>
         </div>
+
+        {/* <div className="col-lg-4 text-center">
+          <img src={profile} alt="profile.png" data-aos="flip-left" data-aos-duration="900"></img>
+        </div> */}
       </div>
     </div>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,21 +5,21 @@ import "../css/home.css";
 import Project from "../components/Project";
 import Contact from "../components/Contact";
 import Navbar from "../components/Navbar";
-import About from "../components/About";
+// import About from "../components/About";
 import Footer from "../components/Footer";
 
 const Home = () => {
   return (
     <>
       <Navbar />
-      <div id="about">
+      {/* <div id="about">
         <About />
+      </div> */}
+      <div id="contact">
+        <Contact />
       </div>
       <div id="projects">
         <Project />
-      </div>
-      <div id="contact">
-        <Contact />
       </div>
       <Footer />
     </>
