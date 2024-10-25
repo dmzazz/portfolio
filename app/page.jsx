fb4b2d5aaeb6d2e07faa46346858a998cef64c36
Dimas Azizir <dimaslaylaa@gmail.com>
2024-10-25 16:45:42 +0700

    migrate to nextjs

---
+++ app/page.jsx
@@ -0,0 +1,121 @@
+import { projects, socialMedia, workExperience } from "@/constant";
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <div className="w-3/4 lg:w-3/4 mx-auto my-6">
+      <section>
+        <div className="flex justify-between items-center">
+          <div className="flex items-center">
+            <div className="w-1 h-6 bg-background-second mr-1 animate-pulse"></div>
+            <h2 className="text-xl font-semibold">
+              Dimas Azizir <span className="bg-white text-main text-sm uppercase p-1 ml-1">hi.</span>
+            </h2>
+          </div>
+          <Link href="https://drive.google.com/file/d/11U-jcIuybeCKDILAfOtCGwD78Z0TPxux/view?usp=drive_link" target="_blank">
+            <button className="bg-background-second hover:bg-[#a0153e] hover:skew-y-3 text-white font-bold py-2 px-4 rounded duration-300">Resume</button>
+          </Link>
+        </div>
+      </section>
+
+      <section>
+        <div className="mt-16">
+          <h2 className="text-lg font-semibold">About.</h2>
+          <p className="w-1/2 mt-4">
+            graduates from <span className="font-semibold">President University</span> who have an interest in the field of <span className="text-red-500 font-semibold">Frontend Engineer</span>
+          </p>
+          <ul className="flex gap-5 mt-2">
+            {socialMedia.map((social, index) => (
+              <li key={index}>
+                <Link href={social.link} target="_blank">
+                  <span className="hover:text-red-500">{social.icon}</span>
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </section>
+
+      {/* <section>
+        <div className="mt-16">
+          <h2 className="text-center text-lg font-semibold">Familiar with</h2>
+          <div className="flex justify-center items-center gap-5 mt-4">
+            {familiarTech.map((tech, index) => (
+              <ul key={index}>
+                <li>
+                  <Image src={tech.icon} alt="tech" width={75} height={75} />
+                </li>
+              </ul>
+            ))}
+          </div>
+        </div>
+      </section> */}
+
+      <section>
+        <div className="mt-16">
+          <h2 className="text-lg font-semibold">Work Experiences.</h2>
+          <div className="relative ml-4">
+            {workExperience.map((work, index) => (
+              <div key={index} className="mt-4 pl-4 relative">
+                {index < workExperience.length && <span className="absolute left-0 top-0 h-full border-l-2 border-red-500" />}
+                <div className="flex justify-between items-center">
+                  <h3 className="font-semibold">{work.company}</h3>
+                  <p>{work.date}</p>
+                </div>
+
+                <p className="text-sm">{work.position}</p>
+                <div className="flex gap-2 mt-2">
+                  {work.tech.map((tech, idx) => (
+                    <span key={idx} className="border border-red-500 text-third rounded-full px-2 text-sm">
+                      {tech}
+                    </span>
+                  ))}
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </section>
+
+      <section>
+        <div className="mt-16">
+          <h2 className="text-lg font-semibold">Projects.</h2>
+          <div className="grid md:grid-cols-2 gap-5 mt-4">
+            {projects.map((project, index) => (
+              <div key={index} className="border p-3 rounded-lg hover:scale-105 duration-300">
+                <div className="flex justify-between items-center">
+                  <h3 className="font-semibold">{project.title}</h3>
+                  <div className="flex items-center gap-2">
+                    <Link href={project.github} target="_blank">
+                      {project.github && (
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="currentColor" className="hover:text-red-500">
+                          <path d="M12 0C5.37 0 0 5.37 0 12c0 5.3 3.44 9.8 8.2 11.39.6.11.82-.26.82-.58v-2.16c-3.34.72-4.04-1.6-4.04-1.6-.55-1.4-1.35-1.78-1.35-1.78-1.1-.75.08-.73.08-.73 1.22.09 1.86 1.26 1.86 1.26 1.08 1.85 2.84 1.31 3.54 1 .11-.78.42-1.31.76-1.61-2.66-.3-5.47-1.33-5.47-5.91 0-1.31.47-2.38 1.24-3.22-.12-.3-.54-1.52.12-3.18 0 0 1.01-.32 3.3 1.23a11.52 11.52 0 013.01-.4c1.02.01 2.05.14 3.01.4 2.3-1.55 3.3-1.23 3.3-1.23.66 1.66.24 2.88.12 3.18.77.84 1.24 1.91 1.24 3.22 0 4.59-2.81 5.61-5.49 5.9.43.37.82 1.1.82 2.23v3.31c0 .32.22.7.83.58C20.56 21.8 24 17.3 24 12 24 5.37 18.63 0 12 0z" />
+                        </svg>
+                      )}
+                    </Link>
+                    <Link href={project.website}>
+                      {project.website && (
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="currentColor" className="hover:text-red-500">
+                          <path d="M10.59 13.41a1 1 0 0 0 1.41 0l4.24-4.24a1 1 0 1 0-1.41-1.41l-4.24 4.24a1 1 0 0 0 0 1.41zm-4.95 6.36A5 5 0 0 1 3 13.66l1.41-1.41a5 5 0 0 1 7.07 0 1 1 0 1 0 1.41-1.41 7 7 0 0 0-9.9 0L1.58 12.24a7 7 0 0 0 0 9.9 7 7 0 0 0 9.9 0l1.41-1.41a1 1 0 1 0-1.41-1.41l-1.41 1.41a5 5 0 0 1-7.07 0zm16.97-16.97a7 7 0 0 0-9.9 0l-1.41 1.41a1 1 0 1 0 1.41 1.41l1.41-1.41a5 5 0 0 1 7.07 0 5 5 0 0 1 0 7.07l-1.41 1.41a5 5 0 0 1-7.07 0 1 1 0 1 0-1.41 1.41 7 7 0 0 0 9.9 0l1.41-1.41a7 7 0 0 0 0-9.9z" />
+                        </svg>
+                      )}
+                    </Link>
+                  </div>
+                </div>
+                <div className="flex gap-2 mt-2">
+                  {project.tech.map((tech, index) => (
+                    <span key={index} className="border border-red-500 text-third rounded-full px-2 text-sm">
+                      {tech}
+                    </span>
+                  ))}
+                </div>
+
+                <p className="mt-4">{project.description}</p>
+              </div>
+            ))}
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+}
---
+++ constant/index.js
@@ -0,0 +1,94 @@
+export const workExperience = [
+  {
+    company: "PT. Bintang Toedjoe",
+    position: "Business Operation Intern",
+    date: "Jan 2024 - Aug 2024",
+    tech: ["TailwindCSS", "NextJS", "DotNet", "MySQL", "UIPath"],
+  },
+  {
+    company: "VOCASIA",
+    position: "Frontend Web Developer Intern",
+    date: "Aug 2023 - Des 2023",
+    tech: ["ReactJS", "JSON Web Server"],
+  },
+];
+
+export const socialMedia = [
+  {
+    title: "Github",
+    icon: (
+      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="currentColor">
+        <path d="M12 0C5.37 0 0 5.37 0 12c0 5.3 3.44 9.8 8.2 11.39.6.11.82-.26.82-.58v-2.16c-3.34.72-4.04-1.6-4.04-1.6-.55-1.4-1.35-1.78-1.35-1.78-1.1-.75.08-.73.08-.73 1.22.09 1.86 1.26 1.86 1.26 1.08 1.85 2.84 1.31 3.54 1 .11-.78.42-1.31.76-1.61-2.66-.3-5.47-1.33-5.47-5.91 0-1.31.47-2.38 1.24-3.22-.12-.3-.54-1.52.12-3.18 0 0 1.01-.32 3.3 1.23a11.52 11.52 0 013.01-.4c1.02.01 2.05.14 3.01.4 2.3-1.55 3.3-1.23 3.3-1.23.66 1.66.24 2.88.12 3.18.77.84 1.24 1.91 1.24 3.22 0 4.59-2.81 5.61-5.49 5.9.43.37.82 1.1.82 2.23v3.31c0 .32.22.7.83.58C20.56 21.8 24 17.3 24 12 24 5.37 18.63 0 12 0z" />
+      </svg>
+    ),
+    link: "https://github.com/dmzazz",
+  },
+  {
+    title: "Linkedln",
+    icon: (
+      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="currentColor">
+        <path d="M19 0h-14c-2.76 0-5 2.24-5 5v14c0 2.76 2.24 5 5 5h14c2.76 0 5-2.24 5-5v-14c0-2.76-2.24-5-5-5zm-11 19h-3v-10h3v10zm-1.5-11.27c-.97 0-1.75-.79-1.75-1.76s.79-1.75 1.75-1.75c.97 0 1.75.79 1.75 1.75s-.79 1.76-1.75 1.76zm13.5 11.27h-3v-5.56c0-1.33-.03-3.05-1.86-3.05-1.86 0-2.14 1.45-2.14 2.95v5.66h-3v-10h2.89v1.37h.04c.4-.75 1.38-1.54 2.84-1.54 3.04 0 3.6 2 3.6 4.56v5.61z" />
+      </svg>
+    ),
+    link: "https://www.linkedin.com/in/dimas-azizir-261661198/",
+  },
+  {
+    title: "Instagram",
+    icon: (
+      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="currentColor">
+        <path d="M12 2.16c3.2 0 3.58.01 4.85.07 1.17.05 1.97.23 2.43.39.61.24 1.05.53 1.52 1 0.47.47.76.91 1 1.52.16.46.34 1.26.39 2.43.06 1.27.07 1.65.07 4.85s-.01 3.58-.07 4.85c-.05 1.17-.23 1.97-.39 2.43-.24.61-.53 1.05-1 1.52-.47.47-.91.76-1.52 1-.46.16-1.26.34-2.43.39-1.27.06-1.65.07-4.85.07s-3.58-.01-4.85-.07c-1.17-.05-1.97-.23-2.43-.39-.61-.24-1.05-.53-1.52-1-.47-.47-.76-.91-1-1.52-.16-.46-.34-1.26-.39-2.43-.06-1.27-.07-1.65-.07-4.85s.01-3.58.07-4.85c.05-1.17.23-1.97.39-2.43.24-.61.53-1.05 1-1.52.47-.47.91-.76 1.52-1 .46-.16 1.26-.34 2.43-.39 1.27-.06 1.65-.07 4.85-.07zm0-2.16c-3.26 0-3.67.01-4.95.07-1.25.06-2.11.25-2.86.54-.79.31-1.47.73-2.13 1.39-.66.66-1.08 1.34-1.39 2.13-.29.75-.48 1.61-.54 2.86-.06 1.27-.07 1.68-.07 4.95s.01 3.67.07 4.95c.06 1.25.25 2.11.54 2.86.31.79.73 1.47 1.39 2.13.66.66 1.34 1.08 2.13 1.39.75.29 1.61.48 2.86.54 1.27.06 1.68.07 4.95.07s3.67-.01 4.95-.07c1.25-.06 2.11-.25 2.86-.54.79-.31 1.47-.73 2.13-1.39.66-.66 1.08-1.34 1.39-2.13.29-.75.48-1.61.54-2.86.06-1.27.07-1.68.07-4.95s-.01-3.67-.07-4.95c-.06-1.25-.25-2.11-.54-2.86-.31-.79-.73-1.47-1.39-2.13-.66-.66-1.34-1.08-2.13-1.39-.75-.29-1.61-.48-2.86-.54-1.27-.06-1.68-.07-4.95-.07zm0 5.84c-3.41 0-6.18 2.76-6.18 6.18s2.76 6.18 6.18 6.18 6.18-2.76 6.18-6.18-2.76-6.18-6.18-6.18zm0 10.2c-2.21 0-4.02-1.8-4.02-4.02s1.8-4.02 4.02-4.02 4.02 1.8 4.02 4.02-1.8 4.02-4.02 4.02zm6.72-11.62c0 .8-.65 1.45-1.45 1.45s-1.45-.65-1.45-1.45.65-1.45 1.45-1.45 1.45.65 1.45 1.45z" />
+      </svg>
+    ),
+    link: "https://www.instagram.com/dimas.azizir/",
+  },
+];
+
+// export const familiarTech = [
+//   {
+//     icon: "/icon/reactjs.png",
+//   },
+//   {
+//     icon: "/icon/nextjs.png",
+//   },
+// ];
+
+export const projects = [
+  {
+    title: "Batch Tracker",
+    description:
+      "Provides an accurate and efficient tracking mechanism in the process of submitting and receiving work orders (WO), raw materials (RM), and production support materials (PM) from the Production Planning and Control (PPIC) division to the Warehouse, to Production.",
+    tech: ["TailwindCSS", "NextJS", "DotNet", "MySQL"],
+    github: "",
+    website: "",
+  },
+  {
+    title: "SelfCare",
+    description:
+      "Self-Care is a website-based information system so that adolescents can gain insight into psychological problems starting from learning to recognize themselves, knowing about what psychology is and the impact that will be received if ignored and understand when to consult psychology.",
+    tech: ["TailwindCSS", "ReactJS", "JSON Server"],
+    github: "https://github.com/dmzazz/capstone-kel-5",
+    website: "",
+  },
+  {
+    title: "Warehouse Management System",
+    description: "An application that functions to record incoming and outgoing goods.",
+    tech: ["ReactJS", "ExpressJS", "NodeJS", "MongoDB"],
+    github: "https://github.com/whend12/Warehouse-Stocktifity",
+    website: "",
+  },
+  {
+    title: "Undangan Digital",
+    description: "An online invitation in the form of a website to make it easier for users to invite relatives to remote weddings or circumcisions.",
+    tech: ["JavaScript", "Bootstrap"],
+    github: "https://github.com/dmzazz/undangankita",
+    website: "https://undangankita.vercel.app",
+  },
+  //   {
+  //     title: "Batch Tracker",
+  //     description:
+  //       "Provides an accurate and efficient tracking mechanism in the process of submitting and receiving work orders (WO), raw materials (RM), and production support materials (PM) from the Production Planning and Control (PPIC) division to the Warehouse, to Production.",
+  //     tech: ["TailwindCSS", "NextJS", "DotNet", "MySQL"],
+  //     github: "",
+  //     website: "",
+  //   },
+];
